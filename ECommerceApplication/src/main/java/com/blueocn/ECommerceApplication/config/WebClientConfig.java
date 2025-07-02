e26735ea5d99c5a6f55c1daf6bf705936f7b88eb
BlueOCN <robgonzalezmu@gmail.com>
2025-07-02 13:14:39 -0600

    Update - JWT added to the ProductWebClient

--- ECommerceApplication/src/main/java/com/blueocn/ECommerceApplication/config/WebClientConfig.java
+++ ECommerceApplication/src/main/java/com/blueocn/ECommerceApplication/config/WebClientConfig.java
@@ -1,19 +1,49 @@
 package com.blueocn.ECommerceApplication.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.reactive.function.client.ClientRequest;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
+
+//    @Bean
+//    public WebClient productWebClient() {
+//        return WebClient.builder()
+//                .baseUrl("http://product-service")
+//                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+//                .build();
+//    }
 
     @Bean
     public WebClient productWebClient() {
         return WebClient.builder()
                 .baseUrl("http://product-service")
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .filter((request, next) -> {
+                    String jwt = extractJwtToken(); // Same logic from above
+                    ClientRequest newRequest = ClientRequest.from(request)
+                            .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwt)
+                            .build();
+                    return next.exchange(newRequest);
+                })
                 .build();
     }
+
+    private String extractJwtToken() {
+        ServletRequestAttributes attrs = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attrs.getRequest();
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            return authHeader.substring(7); // Strip out "Bearer "
+        }
+        throw new RuntimeException("Missing Authorization header");
+    }
+
 }
